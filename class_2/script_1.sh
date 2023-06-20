38171ff2de08c14ec996757ff3b473e92a6e0b69
Abdul <abdugofir00@gmail.com>
2023-06-20 01:46:46 +0000

    commiting class 2 work

--- class_2/script_1.sh
+++ class_2/script_1.sh
@@ -2,9 +2,21 @@
 
 sudo yum update rpm -y; sudo yum update -y 
 
-sudo yum install git -y
+git -v
+status_code=$(echo $?)
+if [ $status_code -eq 0 ]; then
+    echo "Git is already installed!"
+else
+    echo "Installing Git"
+    sudo yum install git -y
+fi
 
-git config --global user.name "Abdul Sharif"
-git config --global user.email "abdugofir00@gmail.com"
+echo "--- To setup your git please enter the following info ---"
+read -p "Enter your name: " name
+read -p "Enter your email: " email
 
-git clone https://github.com/copyPasteNinja/may2023.git /root/may2023
+git config --global user.name $name
+git config --global user.email $email
+
+read -p "Paste your repo link: " repo
+git clone $repo $1 || echo "This is expected"
---
+++ class_2/script_2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Hello there, whats your name? : " name
+echo "Nice to meet you $name"
+ 
+read -p "How old are you $name? : " age
+echo "Wow you are $age years young"
+
